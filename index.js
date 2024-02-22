ba531a161e4aa4083f796cbbf2cdd35a65153ada
ShaliniSinha35 <140596767+ShaliniSinha35@users.noreply.github.com>
2024-02-22 11:02:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mysql = require("mysql");
+const app = express();
+const cors = require('cors');
+app.use(cors())
 const bodyParser = require("body-parser");
 const http = require('http');
 const socketIO = require('socket.io');
 const socketController = require('./controllers/socketController');
-const app = express();
-const cors = require('cors');
-app.use(cors())
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
@@ -20,47 +21,53 @@ io.on('connection', (socket) => {
 
 
 
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "ludo",
 
-  connectionLimit: 10,
-   host:"119.18.54.135",
-   user:"mclinpll_ludo_user",
-   password:"!2MRVcTj4SeB",
-  database:"mclinpll_ludo",
+  // host: "119.18.54.135",
+  // user: "mclinpll_ludo_user",
+  // password: "!2MRVcTj4SeB",
+  // database: "mclinpll_ludo",
 
 
-    // host: "localhost",
-  // user: "root",
-  // password: "",
-  // database: "ludo",
 
 });
 
 
-// connection.connect((err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("connected");
-//   }
-// });
-
-
 function handleDisconnect() {
-  connection.getConnection((err, connection) => {
+  connection.connect((err) => {
     if (err) {
-      console.error('Error getting MySQL connection:', err);
+      console.error('Error connecting to MySQL:', err);
       setTimeout(handleDisconnect, 2000);
     } else {
-      console.log('Connected to MySQL');
-    
-      connection.release();
+      console.log("Connected to MySQL");
     }
   });
 
+  connection.on('error', (err) => {
+    console.error('MySQL connection error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
 }
 
 handleDisconnect();
+
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+
+  } else {
+    console.log("connected");
+  }
+});
 
 app.post("/signup", (req, res) => {
   var name = req.body.name;
@@ -71,74 +78,92 @@ app.post("/signup", (req, res) => {
   var Roboname = "Robo"
 
 
-var sql = `INSERT INTO registration (name, mobile,playerType) VALUES ("${name}", "${phone}","Human")`
+  var sql = `INSERT INTO registration (name, mobile,playerType) VALUES ("${name}", "${phone}","Human")`
   connection.query(sql, function (err, result) {
     if (err) throw err;
-    else{
-        console.log("record inserted");
-        var sql1 = `INSERT INTO registration (name, mobile,playerType) VALUES ("${Roboname}", "${RoboNumber}","Robot")`
+    else {
+      console.log("record inserted");
+      var sql1 = `INSERT INTO registration (name, mobile,playerType) VALUES ("${Roboname}", "${RoboNumber}","Robot")`
       connection.query(sql1, function (err, result) {
-    if (err) throw err;
-    else{
-        console.log("record inserted");
-       
+        if (err) throw err;
+        else {
+          console.log("record inserted");
+
+        }
+
+      });
+      return res.send(result)
     }
-   
-  });
-        return res.send(result)
-    }
-   
+
   });
 });
 
 app.get("/getUserData", (req, res) => {
-  console.log("104",req.query.userId)
-   
-    const userId = req.query.userId;
-    console.log(userId)
-    const sql = `SELECT id, name, mobile FROM registration WHERE mobile = ? `;
-    connection.query(sql,[userId] ,function (err, result) {
-      if (err) throw err;
-      else{
-   
-          return res.send(result)
-      }
-  
-    });
+  console.log("104", req.query.userId)
+
+  const userId = req.query.userId;
+  console.log(userId)
+  const sql = `SELECT id, name, mobile, wallet FROM registration WHERE mobile = ? `;
+  connection.query(sql, [userId], function (err, result) {
+    if (err) throw err;
+    else {
+
+      return res.send(result)
+    }
+
+  });
 });
 
 app.get("/verify", (req, res) => {
-   
-     
-      const userId = req.query.userId;
-      console.log("76",userId)
 
-      const sql = `SELECT id, name, mobile FROM registration WHERE mobile = ${userId}`;
-      connection.query(sql,function (err, result) {
-        if (err) throw err;
-        else{
-            // console.log("verify")
-            console.log(result)
-            return res.send(result)
-        }
-    
-      });
+
+  const userId = req.query.userId;
+  console.log("76", userId)
+
+  const sql = `SELECT id, name, mobile FROM registration WHERE mobile = ${userId}`;
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    else {
+      // console.log("verify")
+      console.log(result)
+      return res.send(result)
+    }
+
+  });
 });
 
 app.post("/changeStatus", (req, res) => {
 
   const userId = req.query.userId;
 
-var sql = `INSERT INTO registration (status) VALUES ("active") WHERE mobile = ?`;
-  connection.query(sql, [userId],function (err, result) {
+  var sql = `INSERT INTO registration (status) VALUES ("active") WHERE mobile = ?`;
+  connection.query(sql, [userId], function (err, result) {
     if (err) throw err;
-    else{
-        console.log("status changed");
-        return res.send(result)
+    else {
+      console.log("status changed");
+      return res.send(result)
     }
-   
+
   });
 });
+
+app.post("/updateWallet", (req, res) => {
+  const userId = req.query.userId; 
+  const wallet = req.query.wallet; 
+  console.log(userId, wallet);
+
+  var sql = `UPDATE registration SET wallet = wallet + ? WHERE mobile = ?`;
+
+  connection.query(sql, [wallet, userId], function (err, result) {
+    if (err) throw err;
+    else {
+      console.log("wallet updated");
+      return res.send(result);
+    }
+  });
+});
+
+
 
 // Start the server
 
